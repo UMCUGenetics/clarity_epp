615408c9d14bf063765491d0da0ba6b6a69aee13
rernst <r.f.ernst-3@umcutrecht.nl>
2017-08-22 16:55:36 +0200

    Fix tecan samplesheet for std controls.

--- samplesheet/tecan.py
+++ samplesheet/tecan.py
@@ -14,7 +14,11 @@ def run_tecan(lims, process_id, output_file):
 
         for placement, artifact in process.output_containers()[0].placements.iteritems():
             placement = ''.join(placement.split(':'))
-            well_plate[placement] = artifact.samples[0].udf['Dx Monsternummer']
+
+            if 'Dx Monsternummer' in artifact.samples[0].udf:
+                well_plate[placement] = artifact.samples[0].udf['Dx Monsternummer']
+            else:
+                well_plate[placement] = artifact.samples[0].name  # Sample is assumed to be tecan control
 
         for well in utils.sort_96_well_plate(well_plate.keys()):
             file.write('{well}\t{sample}\n'.format(
