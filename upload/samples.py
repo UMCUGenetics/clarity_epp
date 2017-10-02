26378bdb40131af7411395d124969318289421e5
Linda Walraven-van Oijen <l.p.a.m.vanoijen@umcutrecht.nl>
2017-10-02 11:01:56 +0200

    update sample upload

--- upload/samples.py
+++ upload/samples.py
@@ -1,12 +1,22 @@
 """Sample upload epp functions."""
 from genologics.entities import Sample, Project, Containertype, Container
 
+hand = False
+
 
 def letter_to_bool(letter):
     """Transform letter (Y/N) to Bool."""
     if letter.upper() == 'J':
         return True
     elif letter.upper() == 'N':
+        return False
+
+
+def value_to_bool(value):
+    """Transform value to Bool."""
+    if len(value) != 0:
+        return True
+    elif len(value) == 0:
         return False
 
 
@@ -17,31 +27,38 @@ def from_helix(lims, input_file):
 
     input_file.readline()  # expect header on first line, skip it.
     for line in input_file:
-        data = line.rstrip().split(';')
-        sample_name = data[0]
+        data = line.rstrip().split('","')
+        sample_name = data[1]
+
+        if value_to_bool(data[5]) == False and letter_to_bool(data[6]) == False and letter_to_bool(data[8]) == False and letter_to_bool(data[9]) == False and data[4] == 'BL':
+            hand = False
+        else:
+            hand = True
 
         if lims.get_sample_number(name=sample_name):
             print "ERROR: Sample {sample_name} already exists.".format(sample_name=sample_name)
         else:
             udf_data = {
                 'Sample Type': 'DNA isolated',  # required lims input
-                'Dx Fractienummer': data[1],
-                'Dx Concentratie (ng/uL)': data[2],
-                'Dx Materiaal type': data[3],
-                #'Dx Foetus': data[4],  # is a date?
-                'Dx Overleden': letter_to_bool(data[5]),
-                'Dx Opslaglocatie': data[6],
-                'Dx Spoed': letter_to_bool(data[7]),
-                'Dx NICU Spoed': letter_to_bool(data[8]),
-                'Dx Persoons ID': data[9],
-                'Dx Werklijstnummer': data[10],
-                'Dx Unummer': data[11],
-                'Dx Geslacht': data[12],
-                'Dx Geboortejaar': data[13],
-                'Dx meet ID': data[14],
-                'Dx Stoftest code': data[15],
-                'Dx Stoftest omschrijving': data[16],
-                'Dx Helix indicatie': data[17],
+                'Dx Onderzoeknummer': data[0].replace('"',''),
+                'Dx Fractienummer': data[2],
+                'Dx Concentratie (ng/uL)': data[3],
+                'Dx Materiaal type': data[4],
+                'Dx Foetus': value_to_bool(data[5]),
+                'Dx Overleden': letter_to_bool(data[6]),
+                'Dx Opslaglocatie': data[7],
+                'Dx Spoed': letter_to_bool(data[8]),
+                'Dx NICU Spoed': letter_to_bool(data[9]),
+                'Dx Persoons ID': data[10],
+                'Dx Werklijstnummer': data[11],
+                'Dx Unummer': data[12],
+                'Dx Geslacht': data[13],
+                'Dx Geboortejaar': data[14],
+                'Dx meet ID': data[15],
+                'Dx Stoftest code': data[16],
+                'Dx Stoftest omschrijving': data[17],
+                'Dx Helix indicatie': data[18].replace('"',''),
+                'Dx Handmatig': hand,
             }
             container = Container.create(lims, type=container_type)
             Sample.create(lims, container=container, position='1:1', project=project, name=sample_name, udf=udf_data)
