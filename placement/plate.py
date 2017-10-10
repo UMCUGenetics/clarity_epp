ffa16d2a55dbcf8e7a1ddb4fc03dfd4fc3149d82
rernst <r.f.ernst-3@umcutrecht.nl>
2017-10-10 14:58:29 +0200

    Fix placements plate and tubes

--- placement/plate.py
+++ placement/plate.py
@@ -1,6 +1,6 @@
 """Plate  placement functions."""
 
-from genologics.entities import Process, Container
+from genologics.entities import Process, Container, Containertype
 
 
 def copy_layout(lims, process_id):
@@ -8,7 +8,11 @@ def copy_layout(lims, process_id):
     process = Process(lims, id=process_id)
 
     # Get parent container layout
-    parent_container = process.parent_processes()[0].output_containers()[0]
+    parent_container = None
+    for container in process.parent_processes()[0].output_containers():
+        if container.type != Containertype(lims, id='2'):  # skip tubes
+            parent_container = container
+
     parent_placements = {}
     for placement in parent_container.placements:
         sample = parent_container.placements[placement].samples[0].name
@@ -18,9 +22,10 @@ def copy_layout(lims, process_id):
     new_container = Container.create(lims, type=parent_container.type)
     placement_list = []
     for artifact in process.all_outputs():
-        placement = parent_placements[artifact.samples[0].name]
-
-        placement_list.append([artifact, (new_container, placement)])
+        sample_name = artifact.samples[0].name
+        if sample_name in parent_placements:
+            placement = parent_placements[sample_name]
+            placement_list.append([artifact, (new_container, placement)])
 
     process.step.placements.set_placement_list(placement_list)
     process.step.placements.post()
