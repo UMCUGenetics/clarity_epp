1ff7c3959e093cf2708e683b0ab939c6e6339578
rernst <r.f.ernst-3@umcutrecht.nl>
2017-10-10 20:04:58 +0200

    Skip placement if not parent plate is found

--- placement/plate.py
+++ placement/plate.py
@@ -14,19 +14,20 @@ def copy_layout(lims, process_id):
             if container.type != Containertype(lims, id='2'):  # skip tubes
                 parent_container = container
 
-    parent_placements = {}
-    for placement in parent_container.placements:
-        sample = parent_container.placements[placement].samples[0].name
-        parent_placements[sample] = placement
+    if parent_container:
+        parent_placements = {}
+        for placement in parent_container.placements:
+            sample = parent_container.placements[placement].samples[0].name
+            parent_placements[sample] = placement
 
-    # Create new container and copy layout
-    new_container = Container.create(lims, type=parent_container.type)
-    placement_list = []
-    for artifact in process.all_outputs():
-        sample_name = artifact.samples[0].name
-        if sample_name in parent_placements:
-            placement = parent_placements[sample_name]
-            placement_list.append([artifact, (new_container, placement)])
+        # Create new container and copy layout
+        new_container = Container.create(lims, type=parent_container.type)
+        placement_list = []
+        for artifact in process.all_outputs():
+            sample_name = artifact.samples[0].name
+            if sample_name in parent_placements:
+                placement = parent_placements[sample_name]
+                placement_list.append([artifact, (new_container, placement)])
 
-    process.step.placements.set_placement_list(placement_list)
-    process.step.placements.post()
+        process.step.placements.set_placement_list(placement_list)
+        process.step.placements.post()
