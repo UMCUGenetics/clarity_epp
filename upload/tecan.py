2bdf59e69226f17e7c0586cc46a58b483bcdc304
rernst <r.f.ernst-3@umcutrecht.nl>
2017-10-10 10:30:36 +0200

    Use api to open the file

--- upload/tecan.py
+++ upload/tecan.py
@@ -8,28 +8,27 @@ def results(lims, process_id):
     process = Process(lims, id=process_id)
     for output in process.shared_result_files():
         if output.name == 'Tecan Spark Output':
-            tecan_result_file = output.files[0].content_location.split('.nl')[1]
+            tecan_result_file = output.files[0]
             tecan_file_order = ['Dx Fluorescentie (nM)', 'Dx Concentratie fluorescentie (ng/ul)', 'sample_name']
             tecan_file_part = -1
 
             sample_measurements = {}
 
-            with open(tecan_result_file, 'r') as f:
-                for line in f:
+            for line in lims.get_file_contents(tecan_result_file.id).split('\n'):
 
-                    if not line.startswith('<>'):
-                        data = line.rstrip().split('\t')
-                        for index, value in enumerate(data[1:]):
-                            coordinate = '{}{}'.format(data[0], str(index))
-                            if coordinate not in sample_measurements:
-                                sample_measurements[coordinate] = {tecan_file_order[tecan_file_part]: float(value)}
-                            elif tecan_file_order[tecan_file_part] == 'sample_name':
-                                sample_measurements[value] = sample_measurements.pop(coordinate)
-                                sample_measurements[value]['coordinate'] = coordinate
-                            else:
-                                sample_measurements[coordinate][tecan_file_order[tecan_file_part]] = float(value)
-                    else:
-                        tecan_file_part += 1
+                if not line.startswith('<>'):
+                    data = line.rstrip().split('\t')
+                    for index, value in enumerate(data[1:]):
+                        coordinate = '{}{}'.format(data[0], str(index))
+                        if coordinate not in sample_measurements:
+                            sample_measurements[coordinate] = {tecan_file_order[tecan_file_part]: float(value)}
+                        elif tecan_file_order[tecan_file_part] == 'sample_name':
+                            sample_measurements[value] = sample_measurements.pop(coordinate)
+                            sample_measurements[value]['coordinate'] = coordinate
+                        else:
+                            sample_measurements[coordinate][tecan_file_order[tecan_file_part]] = float(value)
+                else:
+                    tecan_file_part += 1
 
     for artifact in process.all_outputs():
         if artifact.name != 'Tecan Spark Output' and artifact.name != 'Tecan Spark Samplesheet':
