a3fc090a9d7d8b4f94be361b5124f1796101dd09
rernst <r.f.ernst-3@umcutrecht.nl>
2017-11-29 14:56:11 +0100

    Windows new line

--- export/labels.py
+++ export/labels.py
@@ -7,4 +7,4 @@ def containers(lims, process_id, output_file):
     """Generate container label file."""
     process = Process(lims, id=process_id)
     for container in process.output_containers():
-        output_file.write('{container}\n'.format(container=container.name))
+        output_file.write('{container}\r\n'.format(container=container.name))
