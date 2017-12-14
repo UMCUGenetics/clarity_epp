9635c9fb1ab6584c08fe12906e635214a8213ebe
rernst <r.f.ernst-3@umcutrecht.nl>
2017-12-14 16:14:15 +0100

    New qubit conc. field

--- clarity_epp/qc/qubit.py
+++ clarity_epp/qc/qubit.py
@@ -11,7 +11,7 @@ def set_qc_flag(lims, process_id, cutoff=10):
 
     for artifact in artifacts:
         sample = artifact.samples[0]
-        measurement = artifact.udf['Dx Concentratie fluorescentie (ng/ul)']
+        measurement = artifact.udf['Dx Qubit Concentratie fluorescentie (ng/ul)']
         if sample in samples_measurements:
             samples_measurements[sample].append(measurement)
         else:
@@ -19,10 +19,11 @@ def set_qc_flag(lims, process_id, cutoff=10):
 
     for artifact in artifacts:
         sample = artifact.samples[0]
-        measurement = artifact.udf['Dx Concentratie fluorescentie (ng/ul)']
+        measurement = artifact.udf['Dx Qubit Concentratie fluorescentie (ng/ul)']
 
         sample_measurements = samples_measurements[sample]
         sample_measurements_average = sum(sample_measurements) / float(len(sample_measurements))
+        artifact.udf['Dx Concentratie fluorescentie (ng/ul)'] = sample_measurements_average
 
         cutoff_value = sample_measurements_average * 0.1
 
