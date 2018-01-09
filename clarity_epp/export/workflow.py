a20cd70dc3fad3995217d40674cc57453278585d
rernst <r.f.ernst-3@umcutrecht.nl>
2018-01-09 15:36:04 +0100

    Helix export

--- clarity_epp/export/workflow.py
+++ clarity_epp/export/workflow.py
@@ -8,14 +8,51 @@ def helix(lims, process_id, output_file):
     output_file.write("<meet_id>\tOnderzoeksnummer\tmeetw_zui\tmeetw_zui_herh\tmeetw_libprep\tmeetw_libprep_herh\tmeetw_enrich\tmeetw_enrich_herh\tmeetw_seq\tmeetw_seq_herh\n")
     process = Process(lims, id=process_id)
 
-    for sample in process.all_inputs()[0].samples:
-        output_file.write(
-            "{meet_id}\t{onderzoeksnummer}\tmeetw_zui\tmeetw_zui_herh\tmeetw_libprep\tmeetw_libprep_herh\tmeetw_enrich\tmeetw_enrich_herh\tmeetw_seq\tmeetw_seq_herh\n".format(
-                meet_id=sample.udf['Dx Meet ID'],
-                onderzoeksnummer=sample.udf['Dx Onderzoeknummer'],
-            )
-        )
+    meetw_zui_processes = ['Dx sample registratie zuivering', 'Dx Hamilton uitvullen', 'Dx Hamilton zuiveren', 'Dx Zuiveren gDNA manueel', 'Dx gDNA Normalisatie Caliper']
+    meetw_libprep_processes = ['Dx LibraryPrep Caliper', 'Dx Library Prep amplificatie & clean up']
+    meetw_enrich_processes = ['Dx Multiplexen library prep', 'Dx Enrichment DNA fragments', 'Dx Post Enrichment clean up', 'Dx Post Enrichment PCR & clean up']
+    meetw_seq_processes = ['Dx Library pool verdunnen', 'Dx Multiplexen library pool', 'Dx Library pool denatureren en laden (Nextseq)', 'Dx NextSeq Run (NextSeq)', 'Dx QC controle Lab sequencen']
 
-        # sample_hist = SampleHistory(sample_name=sample.name, lims=lims)
-        # process_type = Processtype(lims, id='354')
-        # print lims.get_artifacts(sample_name=sample.name, process_type=process_type.name)
+    def determin_meetw(meetw_processes, sample_processes):
+        meetw = 0
+        meetw_herh = 0
+
+        for process in meetw_zui_processes:
+            if process in sample_processes:
+                if sample_processes[process] == 1:
+                    meetw = 1
+                else:
+                    meetw = 0
+                    meetw_herh = 1
+                    break
+        return meetw, meetw_herh
+
+    for artifact in process.all_inputs():
+        for sample in artifact.samples:
+            sample_processes = {}
+            sample_history = SampleHistory(lims=lims, sample_name=sample.name, output_artifact=artifact.id)
+
+            for sample_artifact in sample_history.history:
+                for process in sample_history.history[sample_artifact]:
+                    process_name = sample_history.history[sample_artifact][process]['name']
+                    if process_name in sample_processes:
+                        sample_processes[process_name] += 1
+                    else:
+                        sample_processes[process_name] = 1
+
+            # Determine meetw
+            meetw_zui, meetw_zui_herh = determin_meetw(meetw_zui_processes, sample_processes)
+            meetw_libprep, meetw_libprep_herh = determin_meetw(meetw_libprep_processes, sample_processes)
+            meetw_enrich, meetw_enrich_herh = determin_meetw(meetw_enrich_processes, sample_processes)
+            meetw_seq, meetw_seq_herh = determin_meetw(meetw_seq_processes, sample_processes)
+
+            output_file.write(
+                "{meet_id}\t{onderzoeksnummer}\t{meetw_zui}\t{meetw_zui_herh}\t{meetw_libprep}\t{meetw_libprep_herh}\t{meetw_enrich}\t{meetw_enrich_herh}\t{meetw_seq}\t{meetw_seq_herh}\n".format(
+                    meet_id=sample.udf['Dx Meet ID'],
+                    onderzoeksnummer=sample.udf['Dx Onderzoeknummer'],
+                    meetw_zui=meetw_zui, meetw_zui_herh=meetw_zui_herh,
+                    meetw_libprep=meetw_libprep, meetw_libprep_herh=meetw_libprep_herh,
+                    meetw_enrich=meetw_enrich, meetw_enrich_herh=meetw_enrich_herh,
+                    meetw_seq=meetw_seq, meetw_seq_herh=meetw_seq_herh,
+                )
+            )
