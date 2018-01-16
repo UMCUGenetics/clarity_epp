22101077bcc4071c34678082abf3e9e5a5b14d38
rernst <r.f.ernst-3@umcutrecht.nl>
2018-01-16 13:38:20 +0100

    Fix upload

--- clarity_epp/upload/bioanalyzer.py
+++ clarity_epp/upload/bioanalyzer.py
@@ -18,7 +18,7 @@ def results(lims, process_id):
             for line in lims.get_file_contents(bioanalyzer_result_file.id).split('\n'):
                 if line.startswith('Sample Name'):
                     sample = line.rstrip().split(',')[1]
-                elif line.startswith('"Region 1'):
+                elif line.startswith('Region 1'):
                     line = re.sub(r'(""[0-9]+),([0-9\.]+"")', r'\1\2', line)  # Fix
                     size = line.rstrip().split(',')[5]
                     sample_measurements[sample] = int(size)
@@ -26,6 +26,6 @@ def results(lims, process_id):
     # Set UDF
     for artifact in process.all_outputs():
         if artifact.name != 'Bioanalyzer Output' and artifact.name != 'Bioanalyzer Samplesheet':
-            sample_name = artifact.name.split('_')[0]
+            sample_name = artifact.name
             artifact.udf['Dx Fragmentlengte (bp)'] = sample_measurements[sample_name]
             artifact.put()
