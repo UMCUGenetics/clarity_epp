055e358bd597606333c67e9fd161adb2ca1300d8
rernst <r.f.ernst-3@umcutrecht.nl>
2018-01-16 10:12:43 +0100

    Use fractienummer

--- clarity_epp/export/hamilton.py
+++ clarity_epp/export/hamilton.py
@@ -13,7 +13,7 @@ def samplesheet_filling_out(lims, process_id, output_file):
 
     for placement, artifact in process.output_containers()[0].placements.iteritems():
         placement = ''.join(placement.split(':'))
-        well_plate[placement] = artifact.samples[0].name
+        well_plate[placement] = artifact.samples[0].udf['Dx Fractienummer']
 
     for well in utils.sort_96_well_plate(well_plate.keys()):
         output_file.write('{source_tube}\t{well}\n'.format(
@@ -31,7 +31,7 @@ def samplesheet_purify(lims, process_id, output_file):
 
     for placement, artifact in process.output_containers()[0].placements.iteritems():
         placement = ''.join(placement.split(':'))
-        well_plate[placement] = artifact.samples[0].name
+        well_plate[placement] = artifact.samples[0].udf['Dx Fractienummer']
 
     for well in utils.sort_96_well_plate(well_plate.keys()):
         output_file.write('{sample}\t{sample_rack_barcode}\t{sample_rack_position}\t{sample_start_volume}\n'.format(
