72071c68a1b2142b205a7bec653e9e8ebbc0a7c8
rernst <r.f.ernst-3@umcutrecht.nl>
2018-01-19 12:47:27 +0100

    Fix illumina samplesheet sample_ids

--- clarity_epp/export/illumina.py
+++ clarity_epp/export/illumina.py
@@ -27,11 +27,17 @@ def update_samplesheet(lims, process_id, artifact_id, output_file):
             output_file.write('{line}\n'.format(line=line))
         elif header:
             data = line.rstrip().split(',')
+            sample_id_index = header.index('Sample_ID')
             sample_name_index = header.index('Sample_Name')
             sample_project_index = header.index('Sample_Project')
 
+            # Set Sample_Project
             sample_name = re.search('\w*(\d{4}D\d{5})', data[sample_name_index]).group(1)
             data[sample_project_index] = sample_project[sample_name]
+
+            # Overwrite Sample_ID with Sample_name to get correct conversion output folder structure
+            data[sample_id_index] = data[sample_name_index]
+
             output_file.write('{line}\n'.format(line=','.join(data)))
         else:
             output_file.write('{line}\n'.format(line=line))
