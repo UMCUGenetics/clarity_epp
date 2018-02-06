ec3304ef586d0d0b65cb3c6f1c3205b2a6d37d1c
rernst <r.f.ernst-3@umcutrecht.nl>
2018-02-06 15:18:59 +0100

    Calculate weighted average

--- clarity_epp/qc/illumina.py
+++ clarity_epp/qc/illumina.py
@@ -7,5 +7,12 @@ def set_avg_q30(lims, process_id):
     process = Process(lims, id=process_id)
     artifact = process.analytes()[0][0]
 
-    artifact.udf['Dx Average % Bases >=Q30'] = (artifact.udf['% Bases >=Q30 R1'] + artifact.udf['% Bases >=Q30 R2']) / 2.0
+    r1_q30 = artifact.udf['% Bases >=Q30 R1']
+    r1_yield = artifact.udf['Yield PF (Gb) R1']
+    r2_q30 = artifact.udf['% Bases >=Q30 R2']
+    r2_yield = artifact.udf['Yield PF (Gb) R2']
+
+    average_q30 = (r1_q30*r1_yield + r2_q30*r2_yield) / (r1_yield+r2_yield)
+
+    artifact.udf['Dx Average % Bases >=Q30'] = average_q30
     artifact.put()
