7c2a97b6b12956b96bb8142c83c27395afb735a5
rernst <r.f.ernst-3@umcutrecht.nl>
2018-05-03 14:49:39 +0200

    replace shared_result_files with result_files

--- clarity_epp/upload/tapestation.py
+++ clarity_epp/upload/tapestation.py
@@ -9,15 +9,15 @@ def results(lims, process_id):
     sample_measurements = {}
 
     # Parse File
-    for output in process.shared_result_files():
+    for output in process.result_files():
         if output.name == 'TapeStation Output':
             tapestation_result_file = output.files[0]
-
             for line in lims.get_file_contents(tapestation_result_file.id).split('\n'):
                 if line.startswith('FileName'):
                     header = line.split(',')
                     size_index = header.index('Size [bp]')
                     sample_index = header.index('Sample Description')
+
                 elif line:
                     data = line.split(',')
                     sample = data[sample_index]
