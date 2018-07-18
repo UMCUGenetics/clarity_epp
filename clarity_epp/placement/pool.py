a420caaa9bf7fecb81ed6fdbf36a3e3eaf4013ec
rernst <r.f.ernst-3@umcutrecht.nl>
2018-07-18 17:41:43 +0200

    python 2.6 fix

--- clarity_epp/placement/pool.py
+++ clarity_epp/placement/pool.py
@@ -10,10 +10,11 @@ def unpooling(lims, process_id):
     process = Process(lims, id=process_id)
 
     pool_artifact = process.all_inputs()[0]
+
     pool_artifact_demux = lims.get(pool_artifact.uri + '/demux')
     run_id = pool_artifact.name  # Assume run id is set as pool name using placement/artifact/set_runid_name
     sample_artifacts = []  # sample artifacts before pooling
-    for node in pool_artifact_demux.iter('artifact'):
+    for node in pool_artifact_demux.findall('artifact'):
         if node.find('samples'):
             if len(node.find('samples').findall('sample')) == 1:
                 sample_artifact = Artifact(lims, uri=node.attrib['uri'])
