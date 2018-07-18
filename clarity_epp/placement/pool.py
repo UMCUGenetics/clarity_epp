bb53c78368bf3c2c77eb6cb0f2ebf716c3597214
rernst <r.f.ernst-3@umcutrecht.nl>
2018-07-18 17:47:41 +0200

    python 2.6 fix

--- clarity_epp/placement/pool.py
+++ clarity_epp/placement/pool.py
@@ -14,12 +14,11 @@ def unpooling(lims, process_id):
     pool_artifact_demux = lims.get(pool_artifact.uri + '/demux')
     run_id = pool_artifact.name  # Assume run id is set as pool name using placement/artifact/set_runid_name
     sample_artifacts = []  # sample artifacts before pooling
-    for node in pool_artifact_demux.findall('artifact'):
+    for node in pool_artifact_demux.getiterator('artifact'):
         if node.find('samples'):
             if len(node.find('samples').findall('sample')) == 1:
                 sample_artifact = Artifact(lims, uri=node.attrib['uri'])
                 sample_artifact.udf['Dx Sequencing Run ID'] = run_id
                 sample_artifact.put()
                 sample_artifacts.append(sample_artifact)
-
     lims.route_artifacts(sample_artifacts, workflow_uri=Workflow(lims, id=config.post_seq_workflow).uri)  # Dx Post sequencing v1.0
