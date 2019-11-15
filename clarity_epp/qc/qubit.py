d3cfc9c011f8a50e0b3096dc083989978fbc8d49
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2019-11-15 14:40:58 +0100

    Check concentration range.

--- clarity_epp/qc/qubit.py
+++ clarity_epp/qc/qubit.py
@@ -1,5 +1,7 @@
 """Qubit QC epp functions."""
 
+import re
+
 from genologics.entities import Process
 
 
@@ -8,6 +10,8 @@ def set_qc_flag(lims, process_id, cutoff=10):
     process = Process(lims, id=process_id)
     artifacts = process.result_files()
     samples_measurements = {}
+
+    concentration_range = map(int, re.findall('[\d]+', process.udf['Concentratiebereik (ng/ul)']))
 
     for artifact in artifacts:
         sample = artifact.name.rstrip('Qubit').rstrip()
@@ -24,14 +28,17 @@ def set_qc_flag(lims, process_id, cutoff=10):
         sample_measurements_average = sum(sample_measurements) / float(len(sample_measurements))
         artifact.udf['Dx Concentratie fluorescentie (ng/ul)'] = sample_measurements_average
 
-        if len(sample_measurements) == 1:
-            artifact.qc_flag = 'PASSED'
-        elif len(sample_measurements) == 2:
-            sample_measurements_difference = abs(sample_measurements[0] - sample_measurements[1])
-            sample_measurements_deviation = sample_measurements_difference / sample_measurements_average
-
-            if sample_measurements_deviation <= 0.1:
+        if concentration_range[0] <= sample_measurements_average <= concentration_range[1]:
+            if len(sample_measurements) == 1:
                 artifact.qc_flag = 'PASSED'
-            else:
-                artifact.qc_flag = 'FAILED'
+            elif len(sample_measurements) == 2:
+                sample_measurements_difference = abs(sample_measurements[0] - sample_measurements[1])
+                sample_measurements_deviation = sample_measurements_difference / sample_measurements_average
+                if sample_measurements_deviation <= 0.1:
+                    artifact.qc_flag = 'PASSED'
+                else:
+                    artifact.qc_flag = 'FAILED'
+        else:
+            artifact.qc_flag = 'FAILED'
+
         artifact.put()
