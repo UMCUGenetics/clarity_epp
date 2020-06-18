56cf63714e1398145b3100f9925e90fd291380ae
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2020-06-18 11:11:41 +0200

    Better fix for #16

--- clarity_epp/placement/pool.py
+++ clarity_epp/placement/pool.py
@@ -37,9 +37,16 @@ def unpooling(lims, process_id):
         if node.find('samples'):
             if len(node.find('samples').findall('sample')) == 1:
                 sample_artifact = Artifact(lims, uri=node.attrib['uri'])
+                sample = sample_artifact.samples[0] # 1 sample per artifact.
+                
+                # Get sample sequencing run and project from samplesheet
                 sample_artifact.udf['Dx Sequencing Run ID'] = run_id
-                sample_artifact.udf['Dx Sequencing Run Project'] = sample_projects[get_sequence_name(sample_artifact.samples[0])]
+                if 'Sample Type' in sample.udf and 'library' in sample.udf['Sample Type']:  # Use sample.name for external (clarity_portal) samples
+                    sample_artifact.udf['Dx Sequencing Run Project'] = sample_projects[sample.name]
+                else:  # Use sample_artifact.name for Dx samples (upload via Helix)
+                    sample_artifact.udf['Dx Sequencing Run Project'] = sample_projects[sample_artifact.name]
                 sample_artifact.put()
+                
                 if sample_artifact.samples[0].project and sample_artifact.samples[0].project.udf['Application'] == 'DX':  # Only move DX production samples to post sequencing workflow
                     sample_artifacts.append(sample_artifact)
 
