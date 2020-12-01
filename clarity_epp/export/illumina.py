6c25d500d3e8df6024ecdea1e842fcb9347e87d1
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2020-12-01 14:07:33 +0100

    Move space or _ strip to re.sub

--- clarity_epp/export/illumina.py
+++ clarity_epp/export/illumina.py
@@ -67,7 +67,7 @@ def update_samplesheet(lims, process_id, artifact_id, output_file):
                     family = 'GIAB'
                 else:
                     family = sample.project.name
-                    family = re.sub('^dx', '', family, flags=re.IGNORECASE).strip('_ ')  # Remove 'dx' (ignore case) and strip leading space or _
+                    family = re.sub('^dx[ _]*', '', family, flags=re.IGNORECASE)  # Remove 'dx' (ignore case) and strip leading space or _
                 if family not in families:
                     families[family] = {'samples': [], 'NICU': False, 'project_type': family, 'split_project_type': False, 'urgent': False, 'merge': False}
 
