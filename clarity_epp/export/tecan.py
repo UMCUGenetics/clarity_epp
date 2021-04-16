8319785cc5f4410a1578506f38e03b255a06fac6
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2021-04-16 13:26:17 +0200

    Fix tecan scripts for new sample names.

--- clarity_epp/export/tecan.py
+++ clarity_epp/export/tecan.py
@@ -13,7 +13,7 @@ def samplesheet(lims, process_id, output_file):
 
     for placement, artifact in process.output_containers()[0].placements.iteritems():
         placement = ''.join(placement.split(':'))
-        well_plate[placement] = artifact.name
+        well_plate[placement] = artifact.name.split('_')[0]
 
     for well in utils.sort_96_well_plate(well_plate.keys()):
         output_file.write('{well}\t{sample}\n'.format(
