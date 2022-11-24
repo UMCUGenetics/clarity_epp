cf8581d5e88e73c5a43a0f933a1a5c78756534d8
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2022-11-24 14:54:33 +0100

    Change seperator

--- clarity_epp/upload/tecan.py
+++ clarity_epp/upload/tecan.py
@@ -121,9 +121,9 @@ def results_purify_normalise(lims, process_id):
     for result_file in process.result_files():
         if result_file.name == 'Tecan Fluent 480 Output':
             file_data = lims.get_file_contents(result_file.files[0].id).split('\n')
-            header = file_data[0].rstrip().split(',')
+            header = file_data[0].rstrip().split(';')
             for line in file_data[1:]:
-                data = line.rstrip().split(',')
+                data = line.rstrip().split(';')
                 tecan_result[data[header.index('SampleID')]] = {
                     'conc': float(data[header.index('Concentratie')]),
                     'norm': txt_to_bool(data[header.index('Normalisatie')])
