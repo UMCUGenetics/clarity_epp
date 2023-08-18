67facf86b850f331fd5de35c3716390b7a009e27
Linda Walraven-van Oijen <L.P.A.M.vanOijen@umcutrecht.nl>
2023-08-18 13:15:24 +0200

    Switched samples also ok

--- clarity_epp/placement/pipetting.py
+++ clarity_epp/placement/pipetting.py
@@ -30,6 +30,8 @@ def check_nunc_input_nunc_output(lims, process_id):
             if sample_mix:
                 if input_nunc_1 == fraction1 and input_nunc_2 == fraction2 and output_nunc == mix_name:
                     output_artifact.udf['Dx pipetteer check'] = True
+                elif input_nunc_1 == fraction2 and input_nunc_2 == fraction1 and output_nunc == mix_name:
+                    output_artifact.udf['Dx pipetteer check'] = True
                 else:
                     output_artifact.udf['Dx pipetteer check'] = False
             else:
