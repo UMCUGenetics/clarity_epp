890feda60ec49ce95f67330a56ece30f278c25f4
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2024-01-22 14:28:06 +0100

    Add illumina tests

---
+++ tests/test_export_illumina.py
@@ -0,0 +1,21 @@
+from clarity_epp.export import illumina
+
+
+def test_get_override_cycles():
+    # Magnis prep with legacy index settings (8, 8)
+    assert illumina.get_override_cycles([151, 151], [5, 5], [8, 8], [8, 8], 'RC') == 'U5Y145N1;I8;I8;U5Y145N1'
+    assert illumina.get_override_cycles([151, 151], [5, 5], [8, 8], [8, 8], 'F') == 'U5Y145N1;I8;I8;U5Y145N1'
+
+    # Magnis prep with new default index settings (19, 10)
+    assert illumina.get_override_cycles([151, 151], [5, 5], [8, 8], [19, 10], 'RC') == 'U5Y145N1;I8N11;I8N2;U5Y145N1'
+    assert illumina.get_override_cycles([151, 151], [5, 5], [8, 8], [19, 10], 'F') == 'U5Y145N1;I8N11;N2I8;U5Y145N1'
+
+
+def test_get_project():
+    assert illumina.get_project({'SSv7_1': 1, 'SSv7_2': 0, 'SSv7_3': 0}) == 'SSv7_2'
+    assert illumina.get_project({'SSv7_1': 1, 'SSv7_2': 0, 'SSv7_3': 0}, urgent=True) == 'SSv7_1'
+    assert illumina.get_project({'SSv7_1': 3, 'SSv7_2': 1, 'SSv7_3': 0}) == 'SSv7_3'
+    assert illumina.get_project({'SSv7_1': 3, 'SSv7_2': 1, 'SSv7_3': 0}, urgent=True) == 'SSv7_1'
+    assert illumina.get_project({'SSv7_1': 3, 'SSv7_2': 1, 'SSv7_3': 1}) == 'SSv7_2'
+    assert illumina.get_project({'SSv7_1': 3, 'SSv7_2': 1, 'SSv7_3': 0}, urgent=True) == 'SSv7_1'
+    assert illumina.get_project({'SSv7_1': 9, 'SSv7_2': 5, 'SSv7_3': 5}, urgent=True) == 'SSv7_2'
