0c8df893c42f1113d0effab30fc50538f7f3c609
melferink <m.elferink@umcutrecht.nl>
2024-02-08 13:32:13 +0100

    refactor

--- tests/test_export_utils.py
+++ tests/test_export_utils.py
@@ -1,7 +1,5 @@
 import sys
 
-from unittest.mock import patch
-
 from genologics.lims import Lims
 from genologics.entities import Artifact
 
@@ -43,25 +41,31 @@ samples_mock = {}
 samples_mock[sample_name] = MyMock({udf_value: geslacht})
 
 
-@patch('clarity_epp.export.sample.get_samples')
-def test_sample_udf_1(mock_get, capsys):
-    mock_get.return_value = samples_mock
+def test_sample_udf_withudf(mocker, capsys):
+    patched_clarity_epp = mocker.patch(
+        'clarity_epp.export.sample.get_samples',
+        return_value=samples_mock
+    )
     sample.sample_udf("lims", sys.stdout, udf=udf_value, column_name=column_name)
     captured = capsys.readouterr()
     assert captured.out == f"Sample\t{column_name}\n{sample_name}\t{geslacht}\n"
 
 
-@patch('clarity_epp.export.sample.get_samples')
-def test_sample_udf_2(mock_get, capsys):
-    mock_get.return_value = samples_mock
+def test_sample_udf_withoutudf(mocker, capsys):
+    patched_clarity_epp = mocker.patch(
+        'clarity_epp.export.sample.get_samples',
+        return_value=samples_mock
+    )
     sample.sample_udf("lims", sys.stdout, udf="udf2", column_name=column_name)
     captured = capsys.readouterr()
     assert captured.out == f"Sample\t{column_name}\n{sample_name}\tunknown\n"
 
 
-@patch('clarity_epp.export.sample.get_samples')
-def test_sample_udf_3(mock_get, capsys):
-    mock_get.return_value = None
+def test_sample_udf_nosamples(mocker, capsys):
+    patched_clarity_epp = mocker.patch(
+        'clarity_epp.export.sample.get_samples',
+        return_value=None
+    )
     sample.sample_udf("lims", sys.stdout)
     captured = capsys.readouterr()
     assert captured.out == "no_sample_found\n"
