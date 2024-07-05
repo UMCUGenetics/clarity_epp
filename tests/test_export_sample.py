b24f8d0490590c5e17f83ab184a6648b145b1525
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2024-07-05 14:54:00 +0200

    Add tests

--- tests/test_export_sample.py
+++ tests/test_export_sample.py
@@ -1,17 +1,41 @@
 import sys
 
-from clarity_epp.export.sample import sample_udf_dx
+import genologics
+
+from clarity_epp.export.sample import removed_samples, sample_udf_dx
+
+
+class ProjectMock:
+    def __init__(self, name):
+        self.name = name
+        self.udf = {'Application': 'DX'}
 
 
 class SampleMock:
-    def __init__(self, name, udf_dict):
+    def __init__(self, name, udf={}, date_received="2024-01-01"):
         self.name = name
-        self.udf = udf_dict
+        self.udf = udf
+        self.date_received = date_received
+
+
+def test_removed_samples_skip_old_sample(mocker, capsys):
+    sample = SampleMock("test_sample", date_received="2023-01-01")
+    project = ProjectMock('test_project')
+
+    mocker.patch('genologics.lims.Lims.get_projects', return_value=[project])
+    mocker.patch('genologics.lims.Lims.get_samples', return_value=[sample])
+
+    removed_samples(genologics.lims.Lims, sys.stdout)
+    captured = capsys.readouterr()
+    assert captured.out == (
+        'Datum verwijderd\tSample\tSample project\tWerklijst\tOnderzoeks nummer\t'
+        'Onderzoeks indicatie\tVerwijderd uit stap\tStatus\n'
+    )
 
 
 def test_sample_udf_with_udf(mocker, capsys):
     # Test output for sample with known Dx-udf in database
-    sample = SampleMock("test_sample", {"Dx geslacht": "Vrouw"})
+    sample = SampleMock("test_sample", udf={"Dx geslacht": "Vrouw"})
     samples = {sample.name: sample}
     column_name = "test_column"
 
@@ -24,7 +48,7 @@ def test_sample_udf_with_udf(mocker, capsys):
 
 def test_sample_udf_without_udf(mocker, capsys):
     # Test output for sample with no known udf in database
-    sample = SampleMock("test_sample", {"Dx geslacht": "Vrouw"})
+    sample = SampleMock("test_sample", udf={"Dx geslacht": "Vrouw"})
     samples = {sample.name: sample}
     column_name = "test_column"
 
@@ -37,7 +61,7 @@ def test_sample_udf_without_udf(mocker, capsys):
 
 def test_sample_udf_without_dx_udf(mocker, capsys):
     # Test output for sample with known udf in database, but not Dx-udf
-    sample = SampleMock("test_sample", {"Geslacht": "Vrouw"})
+    sample = SampleMock("test_sample", udf={"Geslacht": "Vrouw"})
     samples = {sample.name: sample}
     column_name = "test_column"
 
