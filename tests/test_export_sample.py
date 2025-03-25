4251cf1ae840d88495ff1e264c537d0a1ecf89a7
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2025-03-25 20:00:04 +0100

    Fix udf export if sample is present multiple times in clarity

--- tests/test_export_sample.py
+++ tests/test_export_sample.py
@@ -3,16 +3,16 @@ import sys
 from clarity_epp.export import sample
 
 
-class MyMock:
+class SampleMock:
     def __init__(self, udf):
         self.udf = udf
 
 
 def test_sample_udf_withudf(mocker, capsys):
     # Test output for sample with known Dx-udf in database
-    samples_mock[sample_name] = MyMock({"Dx geslacht": "Vrouw"})
+    samples_mock[sample_name] = [SampleMock({"Dx geslacht": "Vrouw"})]
     patched_clarity_epp = mocker.patch(
-        'clarity_epp.export.sample.get_samples',
+        'clarity_epp.export.sample.get_artifact_samples',
         return_value=samples_mock
     )
     sample.sample_udf_dx("lims", sys.stdout, udf="Dx geslacht", column_name=column_name)
@@ -22,32 +22,43 @@ def test_sample_udf_withudf(mocker, capsys):
 
 def test_sample_udf_withoutudf(mocker, capsys):
     # Test output for sample with no known udf in database
-    samples_mock[sample_name] = MyMock({"Dx geslacht": "Vrouw"})
+    samples_mock[sample_name] = [SampleMock({})]
     patched_clarity_epp = mocker.patch(
-        'clarity_epp.export.sample.get_samples',
+        'clarity_epp.export.sample.get_artifact_samples',
         return_value=samples_mock
     )
-    sample.sample_udf_dx("lims", sys.stdout, udf="udf2", column_name=column_name)
+    sample.sample_udf_dx("lims", sys.stdout, udf="Dx geslacht", column_name=column_name)
     captured = capsys.readouterr()
     assert captured.out == f"Sample\t{column_name}\n{sample_name}\tunknown\n"
 
-def test_sample_udf_withoutdxudf(mocker, capsys):
-    # Test output for sample with known udf in database, but not Dx-udf
-    samples_mock[sample_name] = MyMock({"Geslacht": "Vrouw"})
+def test_sample_udf_with_multiple_equal_values(mocker, capsys):
+    # Test output for sample with no known udf in database
+    samples_mock[sample_name] = [SampleMock({"Dx geslacht": "Vrouw"}), SampleMock({"Dx geslacht": "Vrouw"})]
     patched_clarity_epp = mocker.patch(
-        'clarity_epp.export.sample.get_samples',
+        'clarity_epp.export.sample.get_artifact_samples',
         return_value=samples_mock
     )
-    sample.sample_udf_dx("lims", sys.stdout, udf="Geslacht", column_name=column_name)
+    sample.sample_udf_dx("lims", sys.stdout, udf="Dx geslacht", column_name=column_name)
     captured = capsys.readouterr()
-    assert captured.out == f"Sample\t{column_name}\nWarning, udf is not type \'Dx\'\n"
+    assert captured.out == f"Sample\t{column_name}\n{sample_name}\tVrouw\n"
 
+
+def test_sample_udf_with_multiple_different_values(mocker, capsys):
+    # Test output for sample with no known udf in database
+    samples_mock[sample_name] = [SampleMock({"Dx geslacht": "Vrouw"}), SampleMock({"Dx geslacht": "Man"})]
+    patched_clarity_epp = mocker.patch(
+        'clarity_epp.export.sample.get_artifact_samples',
+        return_value=samples_mock
+    )
+    sample.sample_udf_dx("lims", sys.stdout, udf="Dx geslacht", column_name=column_name)
+    captured = capsys.readouterr()
+    assert captured.out == f"Sample\t{column_name}\n{sample_name}\tmultiple_values_for_udf\n"
 
 def test_sample_udf_nosamples(mocker, capsys):
     # Test output for sample not known in database
-    samples_mock[sample_name] = MyMock({"Dx geslacht": "Vrouw"})
+    samples_mock[sample_name] = [SampleMock({"Dx geslacht": "Vrouw"})]
     patched_clarity_epp = mocker.patch(
-        'clarity_epp.export.sample.get_samples',
+        'clarity_epp.export.sample.get_artifact_samples',
         return_value=None
     )
     sample.sample_udf_dx("lims", sys.stdout)
