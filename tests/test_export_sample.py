a8f002a2b2cd1489197ffbb69dd1545a1a56d9b4
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2025-03-27 11:10:29 +0100

    Add test for multiple samples with one with missing udf

--- tests/test_export_sample.py
+++ tests/test_export_sample.py
@@ -54,6 +54,17 @@ def test_sample_udf_with_multiple_different_values(mocker, capsys):
     captured = capsys.readouterr()
     assert captured.out == f"Sample\t{column_name}\n{sample_name}\tmultiple_values_for_udf\n"
 
+def test_sample_udf_with_multiple_missing_value(mocker, capsys):
+    # Test output for sample with no known udf in database
+    samples_mock[sample_name] = [SampleMock({"Dx geslacht": "Vrouw"}), SampleMock({})]
+    patched_clarity_epp = mocker.patch(
+        'clarity_epp.export.sample.get_artifact_samples',
+        return_value=samples_mock
+    )
+    sample.sample_udf_dx("lims", sys.stdout, udf="Dx geslacht", column_name=column_name)
+    captured = capsys.readouterr()
+    assert captured.out == f"Sample\t{column_name}\n{sample_name}\tVrouw\n"
+
 def test_sample_udf_nosamples(mocker, capsys):
     # Test output for sample not known in database
     samples_mock[sample_name] = [SampleMock({"Dx geslacht": "Vrouw"})]
