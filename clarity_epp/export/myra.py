618255867cad13add06447b2dc8abf8577b3c8b0
Linda Walraven-van Oijen <L.P.A.M.vanOijen@umcutrecht.nl>
2025-07-11 16:17:59 +0200

    Adjust samplesheet for use with new Callisto strip

--- clarity_epp/export/myra.py
+++ clarity_epp/export/myra.py
@@ -25,7 +25,11 @@ def samplesheet_callisto(lims, process_id, output_file):
         "24 Wells Strip Callisto":
         ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8',
          'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8',
-         'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8']
+         'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8'],
+        "24 Wells Strip Callisto barcoded":
+        ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8',
+         'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8',
+         'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8']
     }
 
     output_data = {}
@@ -48,8 +52,9 @@ def samplesheet_callisto(lims, process_id, output_file):
             strip_id = output_name
             type = output_artifact.container.type.name
             strip_well = output_well
-            row = row_number[output_artifact.location[1][0]]
-            label_id = '{strip}_{number}'.format(strip=strip_id, number=row)
+            if "barcoded" not in output_artifact.container.type.name:
+                row = row_number[output_artifact.location[1][0]]
+                label_id = '{strip}_{number}'.format(strip=strip_id, number=row)
 
         if strip_id not in output_data:
             output_data[strip_id] = {"type": type}
