83ca67f810204c74655150c9afed09cd79e4b6a4
kimlensing <kim.lensing@hotmail.com>
2026-02-16 16:46:17 +0100

    syntax error fix

--- clarity_epp/export/email.py
+++ clarity_epp/export/email.py
@@ -30,6 +30,7 @@ def sequencing_run(lims, email_settings, process_id):
     send_email(
         email_settings['server'], email_settings['from'], email_settings['to_sequencing_run_complete'],
         subject, message
+    )
 
 
 
@@ -57,3 +58,4 @@ def send_mail_manager_review(lims, email_settings, process_id):
             manager_subject,
             manager_message
         )
+    
