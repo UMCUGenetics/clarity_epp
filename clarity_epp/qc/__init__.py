6f2db3760b8bf56722153be456dc6a0044790912
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2026-04-17 14:15:44 +0200

    Cleanup

--- clarity_epp/qc/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Automatic QC functions."""
-
-import clarity_epp.qc.bioinformatics
-import clarity_epp.qc.fragment_length
-import clarity_epp.qc.illumina
-import clarity_epp.qc.qubit
-import clarity_epp.qc.sample
--- clarity_epp/upload/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Upload functions."""
-
-import clarity_epp.upload.samples
-import clarity_epp.upload.tecan
-import clarity_epp.upload.tapestation
-import clarity_epp.upload.bioanalyzer
-import clarity_epp.upload.magnis
