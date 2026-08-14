66f0fe91bf3291bfdbbee284a39dc7fd6e0626af
Linda Walraven-van Oijen <L.P.A.M.vanOijen@umcutrecht.nl>
2026-08-14 14:20:27 +0200

    Fix automation Dx Fluent 480 samplesheet uitvullen mengfractie

--- clarity_epp/export/tecan.py
+++ clarity_epp/export/tecan.py
@@ -43,81 +43,82 @@ def samplesheet(lims, process_id, type, output_file):
             ))
 
     elif type == 'filling_out_purify':
-        # Samplesheet Tecan Fluent 480 'Dx Uitvullen en zuiveren' (mix) samples
-        output_file.write(
-            'SourceTubeID;VolSample;VolWater;PositionIndex;MengID\n'
-        )
+        # Get process udf
+        input_ng_udf = process.udf.get('Input (ng) zuivering')
 
-        # Find all QC process types
-        qc_process_types = clarity_epp.export.utils.get_process_types(lims, ['Dx Qubit QC', 'Dx Tecan Spark 10M QC'])
+        if not input_ng_udf:
+            output_file.write('CF "Input (ng) zuivering" is leeg\n')
+        else:
+            # Samplesheet Tecan Fluent 480 'Dx Uitvullen en zuiveren' (mix) samples
+            output_file.write(
+                'SourceTubeID;VolSample;VolWater;PositionIndex;MengID\n'
+            )
 
-        samples = {}
-        # Find concentration in last QC process
-        for input_artifact in process.all_inputs():
-            for input_sample in input_artifact.samples:
-                qc_processes = lims.get_processes(type=qc_process_types, inputartifactlimsid=input_artifact.id)
-                if qc_processes:
-                    qc_process = sorted(qc_processes, key=lambda process: int(process.id.split('-')[-1]))[-1]
-                    for qc_artifact in qc_process.outputs_per_input(input_artifact.id):
-                        if input_sample.name in qc_artifact.name:
-                            for qc_sample in qc_artifact.samples:
-                                if qc_sample.name == input_sample.name:
-                                    concentration = float(qc_artifact.udf['Dx Concentratie fluorescentie (ng/ul)'])
-                                    meter = 'Fluorescentiemeter'
+            # Find all QC process types
+            qc_process_types = clarity_epp.export.utils.get_process_types(lims, ['Dx Qubit QC', 'Dx Tecan Spark 10M QC'])
 
-                else:
-                    parent_process = input_artifact.parent_process
-                    for parent_artifact in parent_process.all_inputs():
-                        if parent_artifact.name == input_sample.name:
-                            qc_processes = lims.get_processes(type=qc_process_types, inputartifactlimsid=parent_artifact.id)
-                            if qc_processes:
-                                qc_process = sorted(qc_processes, key=lambda process: int(process.id.split('-')[-1]))[-1]
-                                for qc_artifact in qc_process.outputs_per_input(parent_artifact.id):
-                                    if input_sample.name in qc_artifact.name:
-                                        for qc_sample in qc_artifact.samples:
-                                            if qc_sample.name == input_sample.name:
-                                                concentration = float(qc_artifact.udf['Dx Concentratie fluorescentie (ng/ul)'])
-                                                meter = 'Fluorescentiemeter'
-                            else:
-                                # No QC process found, use Helix concentration
-                                concentration = input_sample.udf['Dx Concentratie (ng/ul)']
-                                meter = input_sample.udf['Concentratie meting type']
+            samples = {}
+            # Find concentration in last QC process
+            for input_artifact in process.all_inputs():
+                for input_sample in input_artifact.samples:
+                    qc_processes = lims.get_processes(type=qc_process_types, inputartifactlimsid=input_artifact.id)
+                    if qc_processes:
+                        qc_process = sorted(qc_processes, key=lambda process: int(process.id.split('-')[-1]))[-1]
+                        for qc_artifact in qc_process.outputs_per_input(input_artifact.id):
+                            if input_sample.name in qc_artifact.name:
+                                for qc_sample in qc_artifact.samples:
+                                    if qc_sample.name == input_sample.name:
+                                        concentration = float(qc_artifact.udf['Dx Concentratie fluorescentie (ng/ul)'])
+                                        meter = 'Fluorescentiemeter'
 
-                samples[input_sample.udf['Dx Monsternummer']] = {'conc': concentration, 'meter': meter}
+                    else:
+                        parent_process = input_artifact.parent_process
+                        for parent_artifact in parent_process.all_inputs():
+                            if parent_artifact.name == input_sample.name:
+                                qc_processes = lims.get_processes(type=qc_process_types, inputartifactlimsid=parent_artifact.id)
+                                if qc_processes:
+                                    qc_process = sorted(qc_processes, key=lambda process: int(process.id.split('-')[-1]))[-1]
+                                    for qc_artifact in qc_process.outputs_per_input(parent_artifact.id):
+                                        if input_sample.name in qc_artifact.name:
+                                            for qc_sample in qc_artifact.samples:
+                                                if qc_sample.name == input_sample.name:
+                                                    concentration = float(qc_artifact.udf['Dx Concentratie fluorescentie (ng/ul)'])
+                                                    meter = 'Fluorescentiemeter'
+                                else:
+                                    # No QC process found, use Helix concentration
+                                    concentration = input_sample.udf['Dx Concentratie (ng/ul)']
+                                    meter = input_sample.udf['Concentratie meting type']
 
-        for well in clarity_epp.export.utils.sort_96_well_plate(well_plate.keys()):
-            artifact = well_plate[well]
-            input_ng = artifact.udf.get('Input (ng) zuivering')
-            sample_mix = False
-            if len(artifact.samples) > 1:
-                sample_mix = True
+                    samples[input_sample.udf['Dx Monsternummer']] = {'conc': concentration, 'meter': meter}
 
-            artifact_meters = []
-            for sample in artifact.samples:
-                monster = sample.udf['Dx Monsternummer']
-                artifact_meters.append(samples[monster]['meter'])
+            for well in clarity_epp.export.utils.sort_96_well_plate(well_plate.keys()):
+                artifact = well_plate[well]
+                sample_mix = False
+                if len(artifact.samples) > 1:
+                    sample_mix = True
 
-            for sample in artifact.samples:
-                monster = sample.udf['Dx Monsternummer']
-                samples[monster]['message'] = ''
+                artifact_meters = []
+                for sample in artifact.samples:
+                    monster = sample.udf['Dx Monsternummer']
+                    artifact_meters.append(samples[monster]['meter'])
 
-                if sample_mix:
-                    samples[monster]['mix_names'] = artifact.name
-                    # If other sample of smple_mix has a different meter change meter for this sample
-                    if (len(set(artifact_meters)) > 1
-                        and 'Spectrofotometer' in artifact_meters
-                        and samples[monster]['meter'] != 'Spectrofotometer'):
-                        samples[monster]['meter'] = 'Spectrofotometer'
-                else:
-                    samples[monster]['mix_names'] = monster
+                for sample in artifact.samples:
+                    monster = sample.udf['Dx Monsternummer']
+                    samples[monster]['message'] = ''
 
-                if not input_ng:
-                    samples[monster]['message'] = f'CF "Input (ng) zuivering" is leeg voor {artifact.name}'
-                    samples[monster]['sample_volume'] = 'NB'
-                    samples[monster]['water_volume'] = 'NB'
-                else:
+                    if sample_mix:
+                        samples[monster]['mix_names'] = artifact.name
+                        # If other sample of smple_mix has a different meter change meter for this sample
+                        if (len(set(artifact_meters)) > 1
+                            and 'Spectrofotometer' in artifact_meters
+                            and samples[monster]['meter'] != 'Spectrofotometer'):
+                            samples[monster]['meter'] = 'Spectrofotometer'
+                    else:
+                        samples[monster]['mix_names'] = monster
+
                     sample_concentration = samples[monster]['conc']
                     sample_concentration_meter = samples[monster]['meter']
+                    input_ng = int(input_ng_udf.split(' (')[0])
 
                     if sample_concentration_meter == 'Fluorescentiemeter':
                         if sample_mix:  # Mengfractie met Fluorescentiemeter
@@ -147,17 +148,17 @@ def samplesheet(lims, process_id, type, output_file):
                     volume_water = max_volume - volume_sample
                     samples[monster]['water_volume'] = volume_water
 
-            for sample in artifact.samples:
-                monster = sample.udf['Dx Monsternummer']
-                output_file.write('{sample};{volume_sample:.2f};{volume_water:.2f};{index};{name};{empty};{message}\n'.format(
-                    sample=sample.udf['Dx Fractienummer'],
-                    volume_sample=samples[monster]['sample_volume'],
-                    volume_water=samples[monster]['water_volume'],
-                    index=clarity_epp.export.utils.get_well_index(well, one_based=True),
-                    name=samples[monster]['mix_names'],
-                    empty='',
-                    message=samples[monster]['message']
-                ))
+                for sample in artifact.samples:
+                    monster = sample.udf['Dx Monsternummer']
+                    output_file.write('{sample};{volume_sample:.2f};{volume_water:.2f};{index};{name};{empty};{message}\n'.format(
+                        sample=sample.udf['Dx Fractienummer'],
+                        volume_sample=samples[monster]['sample_volume'],
+                        volume_water=samples[monster]['water_volume'],
+                        index=clarity_epp.export.utils.get_well_index(well, one_based=True),
+                        name=samples[monster]['mix_names'],
+                        empty='',
+                        message=samples[monster]['message']
+                    ))
 
     elif type == 'normalise':
         output_file.write('SourceTubeID;PositionID;PositionIndex\n')
