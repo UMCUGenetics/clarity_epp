6b6479b6a2019b6275fdc19cb409488c4b6c68cd
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2026-08-28 15:31:07 +0200

    Fix typo

--- clarity_epp/upload/utils.py
+++ clarity_epp/upload/utils.py
@@ -1,4 +1,5 @@
 """Utility functions."""
+
 from genologics.entities import Workflow
 
 import config
@@ -6,37 +7,37 @@ import config
 
 def txt_to_bool(txt):
     """Transform character (J/N) to Bool."""
-    if txt.upper() == 'J':
+    if txt.upper() == "J":
         return True
-    elif txt.upper() == 'N':
+    elif txt.upper() == "N":
         return False
     elif txt.upper() == "TRUE":
         return True
     elif txt.upper() == "FALSE":
         return False
     else:
-        raise ValueError('Invalid text, only J, N, TRUE or FALSE allowed.')
+        raise ValueError("Invalid text, only J, N, TRUE or FALSE allowed.")
 
 
 def transform_sex(value):
     """Transform helix sex/geslacht value to lims sex/geslacht value."""
     if value.strip():
-        if value.upper() == 'M':
-            return 'Man'
-        elif value.upper() == 'V':
-            return 'Vrouw'
-        elif value.upper() == 'O':
-            return 'Onbekend'
+        if value.upper() == "M":
+            return "Man"
+        elif value.upper() == "V":
+            return "Vrouw"
+        elif value.upper() == "O":
+            return "Onbekend"
         else:
-            raise ValueError('Invalid sex character, only M, V or O allowed.')
+            raise ValueError("Invalid sex character, only M, V or O allowed.")
     else:
         return value
 
 
 def transform_sample_name(value):
     """Transform legacy name to new sample name."""
-    if '/' in value:
-        sample_name = ''.join(value.split('/'))
+    if "/" in value:
+        sample_name = "".join(value.split("/"))
         return sample_name
     else:
         return value
@@ -52,7 +53,7 @@ def stoftestcode_to_workflow(lims, stoftestcode):
 
 def protocol_description_to_workflow(lims, protocol_description):
     """Return workflow based on helix protocoldescription."""
-    if protocol_decsription in config.protocol_description_workflow:
+    if protocol_description in config.protocol_description_workflow:
         return Workflow(lims, id=config.protocol_description_workflow[protocol_description])
     else:
         return None
